1d71748c3fd5d2bcd16e5715e7fbdeb2cac7b3c9
John Lam <johnvinhlam@me.com>
2014-09-12 15:55:51 -0400

    Create Queue.cpp
    C++ version of Queue with comments

--- Queue.cpp
+++ Queue.cpp
@@ -10,60 +10,60 @@ C++
 
 using namespace std;
 
-struct Node 
+struct Node //create node
 {
 	int number;
 	Node* next;
 }*front = NULL, *rear = NULL, *pointer = NULL, *nextp = NULL;
 
-void push(int x)
+void push(int x) // create push function
 {
     nextp = new Node;
     nextp->number = x;
     nextp->next = NULL;
-    if(front == NULL)
+    if(front == NULL) //if front is null, point to next pointer
     {
         front = rear = nextp;
-        rear->next = NULL;
+        rear->next = NULL; // set rear.next as null
     }
     else
     {
-        rear->next = nextp;
-        rear = nextp;
-        rear->next = NULL;
+        rear->next = nextp; //set rear.next to next pointer
+        rear = nextp; //set rear to next pointer
+        rear->next = NULL; //set the rear.next to null
     }
 }
 
 int remove()
 {
     int x;
-    if(front == NULL)
+    if(front == NULL) //if front is empty, queue is empty
     {
         cout<<"empty queue\n";
     }
     else
     {
-        pointer = front;
-        x = pointer->number;
-        front = front->next;
-        delete(pointer);
-        return(x);
+        pointer = front; //set pointer to front
+        x = pointer->number; //set the x (number to removed) to point to the number
+        front = front->next; //set current front to the front's next
+        delete(pointer); //delete the pointer
+        return(x); // return the value delete
     }
 }
 
 int main()
 {
     int n,c = 0,x;
-    cout<<"Enter the number of values to be pushed into queue\n";
+    cout<<"Enter the number of values to be pushed into queue\n"; // ask number of values to enter
     cin>>n;
-    while (c < n)
+    while (c < n) //while numbers of values does not exceed numbers entered, loop
     {
-	cout<<"Enter the value to be entered into queue\n";
+	cout<<"Enter the value to be entered into queue\n"; //ask user to enter in values
 	cin>>x;
-        push(x);
+        push(x); //addes to list
         c++;
      }
-     cout<<"\n\nRemoved Values\n\n";
+     cout<<"\n\nRemoved Values\n\n"; //removes values
      while(true)
      {
         if (front != NULL)
